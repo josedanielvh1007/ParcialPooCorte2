b1bca84435d762cb77fc76cb85c3cd1ac6943765
Jose Daniel <josedanielvh1007@gmail.com>
2025-10-12 21:54:36 -0500

    Utilización de lista para añadir estudiantes y docentes

--- src/main/java/Modelo/Docente.java
+++ src/main/java/Modelo/Docente.java
@@ -10,54 +10,60 @@ import javax.swing.JOptionPane;
  *
  * @author jose
  */
-public class Profesor extends Persona {
-    
+public class Docente extends Persona {
+
     private final int cedula;
     private int pagoHora;
     private String asignatura;
-    
-    public Profesor(int cedula, int pagoMensual, String nombre, String direccion, int telefono, String fechaNacimiento, String asignatura) {
+    private double[] pagos;
+
+    public Docente(int cedula, int pagoMensual, String nombre, String direccion, int telefono, String fechaNacimiento, String asignatura, int horasTrabajadas, int totalClases) {
         super(nombre, direccion, telefono, fechaNacimiento);
         this.cedula = cedula;
         this.pagoHora = pagoMensual;
         this.asignatura = asignatura;
+        this.pagos = calcularPagos(horasTrabajadas, totalClases, this.pagoHora);
     }
-    
+
     public int getCedula() {
         return cedula;
     }
-    
+
     public int getPagoHora() {
         return pagoHora;
     }
-    
+
     public String getAsignatura() {
         return asignatura;
     }
-    
+
     public void setPagoHora(int pagoHora) {
         this.pagoHora = pagoHora;
     }
-    
+
     public void setAsignatura(String asignatura) {
         this.asignatura = asignatura;
     }
-    
+
     @Override
     public String toString() {
         return super.toString() + "\n  - Cédula : " + cedula
                 + "\n  - Pago hora : " + pagoHora
                 + "\n  - Asignatura : " + asignatura;
     }
-    
-    public double[] calcularPagos() {
+
+    private static double[] calcularPagos(int horasTrabajadas, int totalClases, double pagoHora) {
         double[] pagos = new double[2];
-        int horasTrabajadas = Integer.parseInt(
-                JOptionPane.showInputDialog("Horas trabajadas"));
-        int totalClases = Integer.parseInt(
-                JOptionPane.showInputDialog("Clases dadas"));
         pagos[0] = (pagoHora * horasTrabajadas) + (0.2 * totalClases); // Pago al docente
         pagos[1] = 0.17 * pagos[0];
+        return pagos;
+    }
+
+    public void recalcularPagos(int horasTrabajadas, int totalClases) {
+        this.pagos = Docente.calcularPagos(horasTrabajadas, totalClases, this.pagoHora);
+    }
+
+    public double[] getPagos() {
         return pagos;
     }
 }
--- src/main/java/Vista/Main.java
+++ src/main/java/Vista/Main.java
@@ -1,8 +1,11 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package Vista;
+
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
+import Modelo.*;
 
 /**
  *
@@ -10,7 +13,90 @@ package Vista;
  */
 public class Main {
 
+    ArrayList<Docente> docentes = new ArrayList<>();
+    ArrayList<Estudiante> estudiantes = new ArrayList<>();
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Main app = new Main();
+        app.run();
     }
+
+    private void run() {
+        JOptionPane.showMessageDialog(null, "Bienvenido al sistema de notas");
+        int option;
+        do {
+            option = Integer.parseInt(
+                    JOptionPane.showInputDialog("""
+                                            Menu : 
+                                            1. Añadir docente.
+                                            2. Añadir estudiante.
+                                            3. Listar docentes.
+                                            4. Listar estudiantes.
+                                            5. Calcular pagos.
+                                            6. Resumen.
+                                            7. Cargar datos de ejemplo.
+                                            0. Salir."""));
+            switch (option) {
+                case 1 ->
+                    anadirDocente();
+                case 2 ->
+                    anadirEstudiante();
+
+            }
+        } while (option != 0);
+    }
+
+    /*
+    public Docente(int cedula, int pagoMensual, String nombre, String direccion, int telefono, String fechaNacimiento, String asignatura, int horasTrabajadas, int totalClases) {
+        super(nombre, direccion, telefono, fechaNacimiento);
+        this.cedula = cedula;
+        this.pagoHora = pagoMensual;
+        this.asignatura = asignatura;
+        this.pagos = calcularPagos(horasTrabajadas, totalClases, this.pagoHora);
+    }
+     */
+    private Persona anadirPersona() {
+        String nombre = JOptionPane.showInputDialog("Nombre");
+        String direccion = JOptionPane.showInputDialog("Direccion");
+        int telefono = Integer.parseInt(JOptionPane.showInputDialog("Telefono"));
+        String fechaNacimiento = JOptionPane.showInputDialog("Fecha nacimiento ( dd-mm-yyyy )");
+        return new Persona(nombre, direccion, telefono, fechaNacimiento);
+    }
+
+    private void anadirDocente() {
+        Persona base = anadirPersona();
+
+        int cedula = Integer.parseInt(JOptionPane.showInputDialog("Número de cédula"));
+        String asignatura = JOptionPane.showInputDialog("Asignatura");
+        int horasTrabajadas = Integer.parseInt(JOptionPane.showInputDialog("Horas trabajadas"));
+        int totalClases = Integer.parseInt(JOptionPane.showInputDialog("Total clases"));
+        int pagoMensual = Integer.parseInt(JOptionPane.showInputDialog("Pago mensual"));
+
+        docentes.add(new Docente(
+                cedula,
+                pagoMensual,
+                base.getNombre(),
+                base.getDireccion(),
+                base.getTelefono(),
+                base.getFechaNacimiento(),
+                asignatura,
+                horasTrabajadas,
+                totalClases
+        ));
+    }
+
+    private void anadirEstudiante() {
+        Persona base = anadirPersona();
+
+        int grado = Integer.parseInt(JOptionPane.showInputDialog("Grado"));
+
+        estudiantes.add(new Estudiante(
+                base.getNombre(),
+                base.getDireccion(),
+                base.getTelefono(),
+                base.getFechaNacimiento(),
+                grado
+        ));
+    }
+
 }
